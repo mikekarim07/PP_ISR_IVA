c4936095f2116ab6991b53d1e1449a38a19a3958
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2023-07-07 12:28:42 -0600

    Update PP_ISR_IVA.py

--- PP_ISR_IVA.py
+++ PP_ISR_IVA.py
@@ -119,13 +119,13 @@ if Customer_uploaded_file:
     st.write(Customer.shape)
     st.divider()
 
-    def compare_source_tipo(df):
+    def activa(df):
         result = df['Source'] == df['Tipo']
         return result
     aux_bal = pd.concat([Auxiliar, Balanza])
     aux_bal = aux_bal.merge(Catalogo, left_on='Account', right_on='Cuenta', how='left')
     aux_bal = aux_bal[aux_bal['Tipo']!='No Aplica']
-    aux_bal['clas'] = Customer.apply(result, axis=1)   
+    aux_bal['clas'] = Customer.apply(activa, axis=1)   
     alldata = pd.concat([aux_bal,Customer])
     # alldata = alldata[['Account', 'CoCode', 'Source', 'Descripcion', 'Monto']]
     st.dataframe(alldata)
